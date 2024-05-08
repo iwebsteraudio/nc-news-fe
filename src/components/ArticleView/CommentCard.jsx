07387751472ee4b551b82fe0003f218a3205b8f1
iwebsteraudio <iwebster.audio@gmail.com>
2024-05-08 15:39:03 +0100

    Completes ticket 6

---
+++ src/components/ArticleView/CommentCard.jsx
@@ -0,0 +1,15 @@
+const CommentCard = ({ comment }) => {
+
+    console.log(comment)
+
+  return (
+    
+    <div className="comment-card">
+      <p>{comment.body}</p>
+      <p>by {comment.author} at {comment.created_at}</p>
+      <p>{comment.votes} votes</p>
+    </div>
+  );
+};
+
+export default CommentCard;
